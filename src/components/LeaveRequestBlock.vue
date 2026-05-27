4ebebb04cf37927b5fd5f065ed6d4378bb1ce326
andreyka101 <andrejkvasnin236@gmail.com>
2026-05-27 20:56:30 +0300

    0.9.14XS2

--- src/components/LeaveRequestBlock.vue
+++ src/components/LeaveRequestBlock.vue
@@ -1,20 +1,87 @@
 <style lang="scss" scoped>
-div {
+.background_div {
     position: fixed;
     top: 0;
     left: 0;
     width: 100%;
     height: 100%;
-    background-color: rgba(127, 255, 212, 0.269);
+    background-color: #87878733;
     z-index: 10000;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+}
+
+.transparent_box {
+    border: #404040 4px solid;
+    width: 550px;
+    display: flex;
+    flex-direction: column;
+
+    input {
+        color: #ffffffcc;
+        border: #ffffff 0px solid;
+        border-radius: 7px;
+        padding: 7px;
+        font-size: 15px;
+        background-color: #40404052;
+        margin: 10px;
+    }
+
+    button {
+        display: inline-block;
+        border-radius: 8px;
+        padding: 5px;
+        margin: 2px;
+        border: #000 0px solid;
+        // transition: 2s;
+        background-image: repeating-linear-gradient(-45deg, #ceffff, #ceffff 0.5rem, #ffc9c9 0.5rem, #ffc9c9 1rem);
+
+        span {
+            display: inline-block;
+            padding: 8px 10px;
+            color: #FFFFFF;
+            border-radius: 8px;
+            background-color: #404040;
+            transition: 0.6s;
+            font-weight: 900;
+        }
+    }
+
+    button:hover {
+        cursor: pointer;
+
+        span {
+            color: #404040;
+            background-color: #FFC4C4;
+
+        }
+    }
 }
 </style>
 <template>
-    <div>
-        <button @click="click_Back_LeaveRequest">{{ bool_LeaveRequest }}</button>
+    <div class="background_div">
+        <!-- <button @click="click_Back_LeaveRequest">назад X</button> -->
+        <TransparentBox>
+            <h2>
+                Нужен сайт, но не знаете с чего начать?
+            </h2>
+            <p>
+                Задайте вопрос нашему ведущему разработчику. Консультация бесплатна и ни к чему не обязывает.
+            </p>
+            <input type="text">
+            <input type="tel">
+            <button class="active_button">
+                <span>
+                    позвоните мне
+                </span>
+            </button>
+        </TransparentBox>
     </div>
 </template>
 <script setup lang="ts">
+import TransparentBox from './TransparentBox.vue'
+
 const bool_LeaveRequest = defineModel('bool_LeaveRequest');
 const click_Back_LeaveRequest = () => {
     bool_LeaveRequest.value = false
